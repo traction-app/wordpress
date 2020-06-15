12aacad90c91e994584c7acebffb9547810b115e
Lucas Caires <lucas.cairesd@gmail.com>
2020-06-15 13:54:39 -0300

    plugin scaffold

---
+++ traction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Traction
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     traction
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Traction
+ */
+
+// Your code starts here.
